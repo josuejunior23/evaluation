b77c883b90f1e56051b37682a15668d013fd8b51
Yan Kaic <yankaic.as@gmail.com>
2018-11-30 11:13:17 -0300

    modificacoes para teste de fabric e sawtooth

---
+++ benchmark/test/runall.sh
@@ -0,0 +1,16 @@
+node main.js -c teste10.json &&
+node main.js -c teste02.json &&
+node main.js -c teste03.json &&
+node main.js -c teste04.json &&
+node main.js -c teste05.json &&
+node main.js -c teste06.json &&
+node main.js -c teste07.json &&
+node main.js -c teste08.json &&
+node main.js -c teste09.json &&
+node main.js -c teste10.json &&
+node main.js -c teste11.json &&
+node main.js -c teste12.json &&
+node main.js -c teste13.json &&
+node main.js -c teste14.json &&
+node main.js -c teste15.json &&
+node main.js -c teste16.json
